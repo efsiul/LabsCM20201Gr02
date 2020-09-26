c178b58f5befe7aab08992795e64438fba431f67
ebm7pc <ebm7pc@gmail.com>
2020-09-25 19:19:37 -0500

    Lab1 - Logica implementada. Interfaz terminada. Falta validad nombre y apellido

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labsCM20201_gr02/lab1/DummyActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.labsCM20201_gr02.lab1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DummyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dummy)
+    }
+}
