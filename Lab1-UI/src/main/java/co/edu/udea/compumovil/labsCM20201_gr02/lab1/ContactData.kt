abef3d9935979440ea7633c4245ff9c8a7cc9f12
ebm7pc <ebm7pc@gmail.com>
2020-09-27 17:27:42 -0500

    Lab1 - App funcional - Pendiente implementar automplete en país y ciudad

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/labsCM20201_gr02/lab1/ContactData.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labsCM20201_gr02/lab1/ContactData.kt
@@ -2,9 +2,11 @@ package co.edu.udea.compumovil.labsCM20201_gr02.lab1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_contact_data.*
 
 class ContactData : AppCompatActivity() {
 
@@ -15,10 +17,20 @@ class ContactData : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_data)
 
-
         findViewById<Button>(R.id.button_Finalizar).setOnClickListener { agregaInfo(it) }
-
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val telefonoText = telefono.text
+        outState?.putCharSequence("savedText", telefonoText)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val telefonoText = savedInstanceState?.getCharSequence("savedText")
+        telefono.text = telefonoText as Editable?
+    }
+
     private fun agregaInfo(view: View) {
         val inputTextPhone = findViewById<EditText>(R.id.telefono)
         val inputTextEmail = findViewById<EditText>(R.id.email)
@@ -28,7 +40,7 @@ class ContactData : AppCompatActivity() {
         val textPhone= inputTextPhone.text
         val textEmail = inputTextEmail.text
         val textDir= inputTextDir.text
-        val textCiu= inputTextCiu.text
+        val textCiu= ciudad.text
         val autoCompCountry=inputTextCountry.text
 
         if (inputTextPhone.text.toString().isNotEmpty() && inputTextEmail.text.toString().isNotEmpty()&& inputTextCountry.text.toString().isNotEmpty()){
@@ -36,7 +48,7 @@ class ContactData : AppCompatActivity() {
             Log.i("Direccion", textDir.toString())
             Log.i("Email", textEmail.toString())
             Log.i("País",autoCompCountry.toString())
-            Log.i("Ciudad", inputTextCiu.toString())
+            Log.i("Ciudad", textCiu.toString())
 
         }else {
             Toast.makeText(this, "Debe ingresar campos obligatorios", Toast.LENGTH_SHORT).show()
