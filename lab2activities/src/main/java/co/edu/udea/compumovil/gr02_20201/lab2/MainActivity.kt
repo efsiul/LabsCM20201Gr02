aed5339825a248c4da10331b742bd419677a5f2b
ebm7pc <ebm7pc@gmail.com>
2020-10-17 18:15:08 -0500

    Terminadas funcionalidades y pruebas de funcionamiento básico de las vistas del proyecto. Completadas vistas de inicio de sesión, registro, lista de lugares y detalle de lugares. Completadas actividades y fragmentos contenedores de listas de lugares y detalles de cada lugar Completada lógica de actividades: ActivityList.kt, ActivityDetail.kt, FragmentLista.kt, FragmentDetalle.kt, AdaptadorLista.kt y SitiosListener.kt Falta subir al repositorio: lógica para el inicio de sesión desde base de datos, lógica para el registro desde base de datos, imágenes y contenido de lugares elegidos. Falta desarrollar: Usuario puede agregar nuevos lugares de interes a la lista. Control en campos vaciós de inicio de sesión

--- lab2activities/src/main/java/co/edu/udea/compumovil/gr02_20201/lab2/MainActivity.kt
+++ lab2activities/src/main/java/co/edu/udea/compumovil/gr02_20201/lab2/MainActivity.kt
@@ -1,9 +1,8 @@
 package co.edu.udea.compumovil.gr02_20201.lab2
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -21,31 +20,45 @@ class MainActivity : AppCompatActivity() {
         textUser= findViewById<EditText>(R.id.editTextUser)
         textPass= findViewById<EditText>(R.id.editTextPassword)
         findViewById<TextView>(R.id.textViewRegister).setOnClickListener { preparaInfo(it) }
-        findViewById<Button>(R.id.buttonStart).setOnClickListener { validaDatos(it) }
+        findViewById<Button>(R.id.buttonStart).setOnClickListener { controlBotonStart(it) }
     }
 
+    private fun controlBotonStart(view: View){
+        if (validaDatos()){
+            actividadInicio()
+            Toast.makeText(this,"Exito!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     private fun preparaInfo(view: View){
-        siguienteActividad()
+        actividadRegistro()
         Toast.makeText(this, "Hecho!", Toast.LENGTH_SHORT).show()
     }
 
-    private fun validaDatos(view: View){
-        //val retorno=true
-        val c1=textUser.toString()
-        val c2=textPass.toString()
-        if(c1.isNullOrEmpty()||c2.isNullOrEmpty()){
-            textUser.error="jodete"
-            textPass.error="pass malo"
+    private fun validaDatos():Boolean{
+        var retorno=true
+        val c1=textUser.text.toString()
+        val c2=textPass.text.toString()
+        if(c1.isNullOrEmpty()){
+            retorno=false
+            textUser.error="Ingrese un usuario"
+            Toast.makeText(this, "Faltan datos", Toast.LENGTH_SHORT).show()
         }
-        //return retorno
+        if(c2.isNullOrEmpty()){
+            retorno=false
+            textPass.error="Ingrese una contraseña"
+            Toast.makeText(this, "Faltan datos", Toast.LENGTH_SHORT).show()
+        }
+        return retorno
     }
 
 
 
-    
-    
-    fun siguienteActividad(){
+    fun actividadInicio(){
+        val intent : Intent = Intent(this, ActivityList::class.java)
+        startActivity(intent)
+    }
+    fun actividadRegistro(){
         val intent : Intent = Intent(this, ActivityRegistry::class.java)
         startActivity(intent)
     }
